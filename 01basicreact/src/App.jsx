2c9180996180d11733f6f035671d1f900c7b8876
dhananjaykr19 <iamdhananjay1929@gmail.com>
2025-07-15 09:19:06 +0530

    Add counter functionality and custom rendering logic

--- 01basicreact/src/App.jsx
+++ 01basicreact/src/App.jsx
@@ -1,9 +1,26 @@
+import { useState } from "react";
 import Code from "./Code";
 function App() {
+  
+  let [counter, setCounter] = useState(0);
+
+  const addValue = () => {
+    setCounter(counter + 1);
+  }
+  const decValue = () => {
+    if(counter === 0){
+      console.error(`Counter value can't be negative`)
+    }else
+      setCounter(counter - 1);
+  }
+
   return (
     // freagment
     <> 
-      <Code />
+      <h1>Counter value : {counter}</h1>
+      <button onClick={addValue}>Add Value {counter}</button>
+      <br />
+      <button onClick={decValue}>Dec Value {counter}</button>
     </>
   )
 }
